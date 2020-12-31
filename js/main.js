eee3325ebe5cf46dbb27112fe724c26be4b51872
sarinazeitooni <76405658+sarinazeitooni@users.noreply.github.com>
2020-12-31 01:58:01 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,57 @@
-let contactInputNames, contactInputPhoneNumbers, contactJson, contactsArray, personsName, personsPhoneNumber;
+let username, phonenumber, contactsArray;
 let contacts = [];
 let container = document.getElementById("container");
+    
+        function renderContacts(){
+            showContacts();
+        };
 
-    function setValue() {
-        contactInputNames = document.getElementById("name").value;
-        contactInputPhoneNumbers = document.getElementById("phoneNumber").value;
-        pushValue();
-        document.getElementById("name").value = "";
-        document.getElementById("phoneNumber").value = "";
-        settingLocalstorageItems();
-        showContacts();
-    };
+        function setValue() {
+            username = document.getElementById("name").value;
+            phonenumber = document.getElementById("phoneNumber").value;
+            addItem();
+            document.getElementById("name").value = "";
+            document.getElementById("phoneNumber").value = "";
+        };
+        
+        function addItem() {
+            contacts.push({
+                id: contacts.length + 1,
+                name: username,
+                phoneNumber: phonenumber
+            });
+            setLocalStorage(JsonString());
+            
+            showContacts();
+        };
 
-    function pushValue() {
-        contacts.push({
-            id: contacts.length + 1,
-            name: contactInputNames,
-            phoneNumber: contactInputPhoneNumbers
-        });
-        contactJson = JSON.stringify(contacts);
-    };
+        function setLocalStorage(contactJson) {
+            localStorage.setItem("contacts", contactJson);
+        };
 
-    function settingLocalstorageItems() {
-        localStorage.setItem("contacts", contactJson);
-        contactsArray = JSON.parse(localStorage.getItem("contacts"));
-        console.log(localStorage);
-    };
+        function getLocalStorage(){
+            console.log(JSON.parse(localStorage.getItem("contacts")))
+            contactsArray = JSON.parse(localStorage.getItem("contacts"));
+        };
 
-    function showContacts() {
-        contactsArray.map(addContactsByMap);
-        container.innerHTML += `<h4>${personsName} : ${personsPhoneNumber}</h4>`;
-    };
-    function addContactsByMap(person) {
-        personsName = person.name;
-        personsPhoneNumber = person.phoneNumber;
-    };
+        function JsonString(){
+            return  JSON.stringify(contacts);
+        };
+
+        function getLength(){
+            return JSON.parse(localStorage.getItem("contacts")).length;
+        };
+     
+        function showContacts() {
+            debugger
+            getLocalStorage();
+            if(contactsArray.length!==null){
+                contactsArray.map(x=>contacts.push)
+                for(const x of contactsArray){
+                    contacts.push(x);
+                    }
+            };
+
+            contacts.map(person=>{return container.innerHTML += `<h4>${person.name} : ${person.phoneNumber}  --- amount: ${getLength()}</h4> `});
+        };
+
